735f2d703b9777e65fbd6bbae483abd4331cf213
Virtuoel <Virtuoel@users.noreply.github.com>
2021-05-21 03:22:05 +0300

    Fix crash

--- src/main/java/virtuoel/statement/mixin/sync/BlockUpdateS2CPacketMixin.java
+++ src/main/java/virtuoel/statement/mixin/sync/BlockUpdateS2CPacketMixin.java
@@ -1,6 +1,8 @@
 package virtuoel.statement.mixin.sync;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +18,7 @@ import virtuoel.statement.Statement;
 @Mixin(BlockUpdateS2CPacket.class)
 public class BlockUpdateS2CPacketMixin
 {
-	@Shadow BlockState state;
+	@Shadow @Final @Mutable BlockState state;
 	
 	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)V")
 	private void onConstruct(BlockView world, BlockPos pos, CallbackInfo info)
