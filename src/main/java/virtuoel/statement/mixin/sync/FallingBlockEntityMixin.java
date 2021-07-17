168bfbfc621d2bc41165236f89973d1414b6ad13
Virtuoel <Virtuoel@users.noreply.github.com>
2021-07-17 18:10:05 +0300

    Fix potential wrong state showing up for falling blocks

---
+++ src/main/java/virtuoel/statement/mixin/sync/FallingBlockEntityMixin.java
@@ -0,0 +1,23 @@
+package virtuoel.statement.mixin.sync;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.FallingBlockEntity;
+import virtuoel.statement.Statement;
+
+@Mixin(FallingBlockEntity.class)
+public abstract class FallingBlockEntityMixin
+{
+	@Shadow abstract BlockState getBlockState();
+	
+	@ModifyArg(method = "createSpawnPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/EntitySpawnS2CPacket;<init>(Lnet/minecraft/entity/Entity;I)V"))
+	private int createSpawnPacketGetRawIdFromStateModify(int id)
+	{
+		return Statement.getSyncedStateId(Block.STATE_IDS, getBlockState()).orElse(id);
+	}
+}
