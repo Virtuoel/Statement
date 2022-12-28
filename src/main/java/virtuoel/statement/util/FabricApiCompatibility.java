810f9aa7e1e072b8402f407281049bbc6431d317
Virtuoel <Virtuoel@users.noreply.github.com>
2022-12-28 05:52:17 +0300

    Fix dedicated server mixin crash
    Closes #19

--- src/main/java/virtuoel/statement/util/FabricApiCompatibility.java
+++ src/main/java/virtuoel/statement/util/FabricApiCompatibility.java
@@ -36,12 +36,24 @@ import net.minecraft.world.BlockView;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.loading.FMLLoader;
 import virtuoel.statement.Statement;
+import virtuoel.statement.api.StateRefresher;
 
 @EventBusSubscriber(modid = Statement.MOD_ID)
 public class FabricApiCompatibility
 {
+	@SubscribeEvent
+	public static void onServerStarting(final FMLServerStartingEvent event)
+	{
+		if (event.getServer().isDedicated())
+		{
+			StateRefresher.INSTANCE.reorderBlockStates();
+			StateRefresher.INSTANCE.reorderFluidStates();
+		}
+	}
+	
 	@SubscribeEvent
 	public static void onRegisterCommands(RegisterCommandsEvent event)
 	{
